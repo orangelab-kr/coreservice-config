c66dab783bfe0834efe342eacd8e8f3551dbc283
bakjonghun <bakjonghun@hikick.kr>
2021-09-22 12:10:47 +0900

    add: prisma

---
+++ src/tools/database.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+prisma.$use(async (params, next) => {
+  const bypassSoftDeleted: string[] = [];
+  if (params.model && !bypassSoftDeleted.includes(params.model)) {
+    if (!['create', 'update', 'upsert', 'delete'].includes(params.action)) {
+      if (!params.args.where) {
+        params.args.where = {};
+      }
+
+      if (!params.args.where['deletedAt']) {
+        params.args.where['deletedAt'] = null;
+      }
+    }
+
+    if (['delete', 'deleteMany'].includes(params.action)) {
+      switch (params.action) {
+        case 'delete':
+          params.action = 'update';
+          break;
+        case 'deleteMany':
+          params.action = 'updateMany';
+          break;
+      }
+
+      if (!params.args.data) {
+        params.args.data = {};
+      }
+
+      params.args.data['deletedAt'] = new Date();
+    }
+  }
+
+  return next(params);
+});
+
+export { prisma };
